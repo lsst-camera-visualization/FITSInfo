4753c16da410e1bafe19e9670b999926e3b75a61
Tony Johnson <tonyj@stanford.edu>
2023-05-29 08:08:11 -0700

    Add webhook for update

--- src/main/java/org/lsst/fits/fitsinfo/WebHook.java
+++ src/main/java/org/lsst/fits/fitsinfo/WebHook.java
@@ -2,6 +2,8 @@ package org.lsst.fits.fitsinfo;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -20,6 +22,8 @@ import org.lsst.ccs.imagenaming.ImageName;
 @Path("webhook")
 public class WebHook {
 
+    private static final Logger LOG = Logger.getLogger(WebHook.class.getName());
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -32,4 +36,19 @@ public class WebHook {
             return Response.serverError().entity(Collections.singletonMap("error", x.getMessage())).build();
         }
     }
+
+    @POST
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@Context Sse sse, Map<String, String> data) {
+        try {
+            ImageName image = new ImageName(data.get("exp_id"));
+            LOG.log(Level.INFO, "Webhook update: {0} {1}", new Object[]{image, data});
+            return Response.ok().build();
+        } catch (RuntimeException x) {
+            return Response.serverError().entity(Collections.singletonMap("error", x.getMessage())).build();
+        }
+    }
+
 }
